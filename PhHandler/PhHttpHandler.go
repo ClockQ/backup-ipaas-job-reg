a321a6c0e906e36719fa98c57d84564fb3691a4f
clock <2567729201@qq.com>
2019-08-30 11:54:12 +0800

    对接 TM 计算

--- PhHandler/PhHttpHandler.go
+++ PhHandler/PhHttpHandler.go
@@ -2,14 +2,14 @@ package PhHandler
 
 import (
 	"encoding/json"
+	"github.com/PharbersDeveloper/ipaas-job-reg/PhJobReg"
 	"github.com/PharbersDeveloper/ipaas-job-reg/PhMessage"
 	"github.com/PharbersDeveloper/ipaas-job-reg/PhModel"
 	"io/ioutil"
 	"net/http"
-	"os"
 )
 
-func PhHttpHandler(kh *PhMessage.PhKafkaHandler, _ *PhMessage.PhMqttHandler) (_ func(http.ResponseWriter, *http.Request)) {
+func PhHttpHandler(kh *PhMessage.PhKafkaHandler, mh *PhMessage.PhMqttHandler) (_ func(http.ResponseWriter, *http.Request)) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var response []byte
 
@@ -22,14 +22,9 @@ func PhHttpHandler(kh *PhMessage.PhKafkaHandler, _ *PhMessage.PhMqttHandler) (_ 
 				response = []byte("Param Parse Error: " + err.Error())
 			}
 
-			//jobRequestTopic := os.Getenv("JOB_REQUEST_TOPIC")
-			//jobRequest := PhModel.JobRequest{}.GenTestData()
-			//err = kh.Send(jobRequestTopic, jobRequest)
-			connectRequestTopic := os.Getenv("CONNECT_REQUEST_TOPIC")
-			connectRequest := PhModel.ConnectRequest{}.GenTestData()
-			err = kh.Send(connectRequestTopic, connectRequest)
+			err = PhJobReg.PhJobReg(model, kh, mh)
 			if err != nil {
-				response = []byte("Kafka Send Error: " + err.Error())
+				response = []byte("Job Reg Error: " + err.Error())
 			}
 
 			response = []byte("The Call Is Successful")
--- PhModel/JobRequest.go
+++ PhModel/JobRequest.go
@@ -2,6 +2,7 @@ package PhModel
 
 type JobRequest struct {
 	*PhSchemaModel
+	Name           string
 	JobType        string // Jar | Py | R
 	Class          string
 	Master         string
@@ -11,13 +12,14 @@ type JobRequest struct {
 	NumExecutors   string
 	Queue          string
 	Target         string
-	File           string
-	Parameter      string
+	Files          string
+	Parameters     string
 }
 
 // TODO: 注意删除
 func (model JobRequest) GenTestData() *JobRequest {
 	model.PhSchemaModel = &PhSchemaModel{}
+	model.Name = "TestJob"
 	model.JobType = "Jar"
 	model.Class = "com.pharbers.ipaas.data.driver.Main"
 	model.Master = "yarn"
@@ -27,7 +29,24 @@ func (model JobRequest) GenTestData() *JobRequest {
 	model.NumExecutors = "2"
 	model.Queue = "default"
 	model.Target = "hdfs:///jars/context/job-context.jar"
-	model.File = "hdfs:///jars/context/pharbers_config/kafka_config.xml,hdfs:///jars/context/pharbers_config/secrets/kafka.broker1.truststore.jks,hdfs:///jars/context/pharbers_config/secrets/kafka.broker1.keystore.jks"
-	model.Parameter = "yaml hdfs hdfs:///test/MZclean.yaml"
+	model.Files = "hdfs:///jars/context/pharbers_config/kafka_config.xml,hdfs:///jars/context/pharbers_config/secrets/kafka.broker1.truststore.jks,hdfs:///jars/context/pharbers_config/secrets/kafka.broker1.keystore.jks"
+	model.Parameters = "yaml hdfs hdfs:///test/MZclean.yaml"
+	return &model
+}
+
+// TODO: 注意删除
+func (model JobRequest) GenTMData() *JobRequest {
+	model.PhSchemaModel = &PhSchemaModel{}
+	model.Name = "TM Calc"
+	model.JobType = "R"
+	model.Master = "yarn"
+	model.DeployMode = "cluster"
+	model.ExecutorMemory = "1G"
+	model.ExecutorCores = "1"
+	model.NumExecutors = "2"
+	model.Queue = "researches"
+	model.Target = "hdfs:///jars/context/NTMR/TMUCBCalMain.R"
+	model.Files = "hdfs:///jars/context/pharbers_config/kafka_config.xml,hdfs:///jars/context/pharbers_config/secrets/kafka.broker1.truststore.jks,hdfs:///jars/context/pharbers_config/secrets/kafka.broker1.keystore.jks"
+	model.Parameters = "hdfs://192.168.100.137:9000//test/UCBTest/inputParquet/TMInputParquet0820/cal_data hdfs://192.168.100.137:9000//test/UCBTest/inputParquet/TMInputParquet0820/weightages hdfs://192.168.100.137:9000//test/UCBTest/inputParquet/TMInputParquet0820/curves-n hdfs://192.168.100.137:9000//test/UCBTest/inputParquet/TMInputParquet0820/competitor"
 	return &model
 }
